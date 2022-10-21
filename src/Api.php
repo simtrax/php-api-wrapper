52a628320e2396ae49bdd00fea2ab2c182f3f472
Simon Månsson <simon.mansson@niftitech.com>
2022-10-21 14:52:54 +0200

    If Lime, do some hacks.

--- src/Api.php
+++ src/Api.php
@@ -48,6 +48,13 @@ class Api
      */
     public function __call($name, $arguments)
     {
+		if(class_basename($this->transport) == 'LimeCRM' && 
+			isset($arguments[0]['limit'])
+		) {
+			$arguments[0]['_limit'] = $arguments[0]['limit'];
+			unset($arguments[0]['limit']);
+		}
+
         if (method_exists($this, $name)) {
             return $this->$name(...$arguments);
         }
