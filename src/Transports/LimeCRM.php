1302154252163b2114e853f41939a09f281fdb8b
Simon Månsson <simon.mansson@niftitech.com>
2022-10-21 10:28:15 +0200

    Adding alternative Transport.

---
+++ src/Transports/LimeCRM.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Cristal\ApiWrapper\Transports;
+
+use Curl\Curl as CurlClient;
+
+class Bearer extends Transport
+{
+    /**
+     * @var string
+     */
+    protected $token;
+
+    /**
+     * Curl constructor.
+     *
+     * @param string     $token
+     * @param string     $entrypoint
+     * @param CurlClient $client
+     */
+    public function __construct(string $token, string $entrypoint, CurlClient $client)
+    {
+        parent::__construct($entrypoint, $client);
+        $this->token = $token;
+        $this->getClient()->setHeader('x-api-key', $this->getToken());
+    }
+
+    public function getToken()
+    {
+        return $this->token;
+    }
+}
